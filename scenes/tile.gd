ed7f80cdbd31edf8e485781a4464fef018001b71
Dead-Vortex <milesohalpin@gmail.com>
2026-05-16 17:27:53 -0700

    tiles can be generated through code, number is now a text label
    probably more things but im hella lazy go look through the commit yourself

--- scenes/tile.gd
+++ scenes/tile.gd
@@ -1,15 +1,22 @@
 extends Control
 
-@onready var number = $Number
-@onready var letter = $Letter
+@onready var tile_number = $Number
+@onready var tile_letter = $Letter
 var letterValues = {"A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4, "I": 1, "J": 8, "K": 5, "L": 1, "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4, "Z": 10}
 var numToLetter = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H", 9: "I", 10: "J", 11: "K", 12: "L", 13: "M", 14: "N", 15: "O", 16: "P", 17: "Q", 18: "R", 19: "S", 20: "T", 21: "U", 22: "V", 23: "W", 24: "X", 25: "Y", 26: "Z"}
 var letterToNum = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12, "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24, "Y": 25, "Z": 26}
 #var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+var number: int = 1
+var letter: int = 1
+
 func _ready() -> void:
 	#for i in 26:
 		#numToLetter[i + 1] = letters[i]
 		#letterToNum[letters[i]] = i + 1
-	letter.frame = randi_range(1, 26)
-	number.frame = letterValues[numToLetter[letter.frame]] + 26
+	letter = randi_range(1, 26)
+	number = letterValues[numToLetter[letter]]
+	name = numToLetter[letter]
+	tile_letter.frame = letter
+	tile_number.text = str(number)
+	tooltip_text = "\"" + numToLetter[letter] + "\"\n" + str(number) + " Point" + ("s" if number != 1 else "")
