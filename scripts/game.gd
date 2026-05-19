c43f7eb8ab21c06e8169f70aedb488f025ef2dff
Dead-Vortex <milesohalpin@gmail.com>
2026-05-18 18:57:44 -0700

    can spell words and return tiles to hand
    - moved letter values/letter num lookup tables to a class script whatever its called
    - disabled anti-aliasing for number font
    - most likely a couple other very small changes

--- scripts/game.gd
+++ scripts/game.gd
@@ -28,9 +28,12 @@ func _ready() -> void:
 func _process(_delta) -> void:
 	handCounter.text = str(hand.get_child_count()) + "/7"
 
-func draw_new_tile() -> void:
+func draw_new_tile(letter = "") -> void:
 	var tile_instance = tile_scene.instantiate()
 	hand.add_child(tile_instance)
+	tile_instance.tile_clicked.connect(_on_tile_clicked)
+	if letter != "":
+		tile_instance.change_letter(letter)
 	sort_hand()
 
 func sort_hand() -> void:
@@ -39,7 +42,13 @@ func sort_hand() -> void:
 	for i in len(tiles_to_be_sorted):
 		hand.move_child(tiles_to_be_sorted[i], i)
 
-	#print("d")
-	#typed_word += str(letter.name)[0]
-	#word_display.text = typed_word
-	#letter.queue_free()
+func _on_tile_clicked(clicked_tile) -> void:
+	typed_word += Letters.NUM_TO_LETTER[clicked_tile.letter]
+	word_display.text = typed_word + "\n" + str(in_dictionary(typed_word))
+	clicked_tile.queue_free()
+
+func _on_word_cleared() -> void:
+	for i in len(typed_word):
+		draw_new_tile(typed_word[i])
+	typed_word = ""
+	word_display.text = typed_word
--- scripts/tile.gd
+++ scripts/tile.gd
@@ -2,9 +2,9 @@ extends Control
 
 @onready var tile_number = $Number
 @onready var tile_letter = $Letter
-var letterValues = {"A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4, "I": 1, "J": 8, "K": 5, "L": 1, "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4, "Z": 10}
-var numToLetter = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H", 9: "I", 10: "J", 11: "K", 12: "L", 13: "M", 14: "N", 15: "O", 16: "P", 17: "Q", 18: "R", 19: "S", 20: "T", 21: "U", 22: "V", 23: "W", 24: "X", 25: "Y", 26: "Z"}
-var letterToNum = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12, "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24, "Y": 25, "Z": 26}
+#var letterValues = {"A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4, "I": 1, "J": 8, "K": 5, "L": 1, "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4, "Z": 10}
+#var numToLetter = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H", 9: "I", 10: "J", 11: "K", 12: "L", 13: "M", 14: "N", 15: "O", 16: "P", 17: "Q", 18: "R", 19: "S", 20: "T", 21: "U", 22: "V", 23: "W", 24: "X", 25: "Y", 26: "Z"}
+#var letterToNum = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12, "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24, "Y": 25, "Z": 26}
 #var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 var number: int = 1
@@ -12,19 +12,24 @@ var letter: int = 1
 
 @onready var soundplayer = $AudioStreamPlayer
 
+signal tile_clicked(tile)
+
 func _ready() -> void:
 	#for i in 26:
 		#numToLetter[i + 1] = letters[i]
 		#letterToNum[letters[i]] = i + 1
-	letter = randi_range(1, 26)
-	number = letterValues[numToLetter[letter]]
-	name = numToLetter[letter]
-	tile_letter.frame = letter
-	tile_number.text = str(number)
-	tooltip_text = "\"" + numToLetter[letter] + "\"\n" + str(number) + " Point" + ("s" if number != 1 else "")
+	change_letter(Letters.NUM_TO_LETTER[randi_range(1, 26)])
 	soundplayer.pitch_scale = randf_range(0.8, 1.1)
 	soundplayer.play()
 
+func change_letter(new_letter: String):
+	letter = Letters.LETTER_TO_NUM[new_letter]
+	number = Letters.LETTER_VALUES[Letters.NUM_TO_LETTER[letter]]
+	name = Letters.NUM_TO_LETTER[letter]
+	tile_letter.frame = letter
+	tile_number.text = str(number)
+	tooltip_text = "\"" + Letters.NUM_TO_LETTER[letter] + "\"\n" + str(number) + " Point" + ("s" if number != 1 else "")
 
 func _on_tile_clicked() -> void:
 	print("hello from " + name)
+	emit_signal("tile_clicked", self)
